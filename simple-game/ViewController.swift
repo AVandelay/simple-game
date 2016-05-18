f3bb9fb4e4535f995ce55ab00a8e8e3716970b2f
westdorp <westdorp@gmail.com>
2016-05-18 16:09:10 -0700

    Initial Commit

---
+++ simple-game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simple-game
+//
+//  Created by kentaro on 5/18/16.
+//  Copyright © 2016 ken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
