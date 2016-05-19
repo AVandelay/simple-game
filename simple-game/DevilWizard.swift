37fb7f24b210703c1ffdfa239668d0a46167a01d
westdorp <westdorp@gmail.com>
2016-05-18 20:32:23 -0700

    first commit

---
+++ simple-game/DevilWizard.swift
@@ -0,0 +1,9 @@
+//
+//  DevilWizard.swift
+//  simple-game
+//
+//  Created by kentaro on 5/18/16.
+//  Copyright © 2016 ken. All rights reserved.
+//
+
+import Foundation
---
+++ simple-game/Kimara.swift
@@ -0,0 +1,9 @@
+//
+//  Kimara.swift
+//  simple-game
+//
+//  Created by kentaro on 5/18/16.
+//  Copyright © 2016 ken. All rights reserved.
+//
+
+import Foundation
